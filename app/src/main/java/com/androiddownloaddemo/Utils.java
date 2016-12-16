3679eb79cfeb5e746f662f468c28b64676e756ee
TusharMann <tusharmann29@gmail.com>
2016-12-16 14:26:18 +0530

    Pdf downloader

---
+++ app/src/main/java/com/androiddownloaddemo/Utils.java
@@ -0,0 +1,16 @@
+package com.androiddownloaddemo;
+
+/**
+ * Created by SONU on 29/10/15.
+ */
+public class Utils {
+
+    //String Values to be Used in App
+    public static final String downloadDirectory = "Androhub Downloads";
+    public static final String mainUrl = "http://sol.du.ac.in/admission/";
+    public static final String downloadPdfUrl = "http://sol.du.ac.in/admission/FeeReceipt_Dashboard.aspx?&SOL_ROLL_NO=04-1-02-000776&RECEIPT_NO=07-01-065231";
+    public static final String downloadDocUrl = "http://androhub.com/demo/demo.doc";
+    public static final String downloadZipUrl = "http://androhub.com/demo/demo.zip";
+    public static final String downloadVideoUrl = "http://androhub.com/demo/demo.mp4";
+    public static final String downloadMp3Url = "http://androhub.com/demo/demo.mp3";
+}
